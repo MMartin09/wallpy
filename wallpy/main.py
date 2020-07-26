3c869aa066b70b3be45fb7d0d87426d81f8d858b
MMartin09 <mosermartin09@gmail.com>
2020-07-26 19:10:56 +0200

    Implement basic function to set the wallpaper

--- wallpy/main.py
+++ wallpy/main.py
@@ -1,7 +1,11 @@
+from wallpy.wallpaper import set_wallpaper
 
 
 def main():
-    pass
+    wallpaper_file_1: str = "images/breno-machado-in9-n0JwgZ0-unsplash.jpg"
+    wallpaper_file_2: str = "images/jonatan-pie-3l3RwQdHRHg-unsplash.jpg"
+
+    set_wallpaper(wallpaper_file_2)
 
 
 if __name__ == '__main__':
