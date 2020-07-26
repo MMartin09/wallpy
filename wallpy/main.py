62331ac23e3a4d48a39342592e1e9ac5226ac426
MMartin09 <mosermartin09@gmail.com>
2020-07-26 21:15:58 +0200

    implement test to download NASA image of the day

--- wallpy/main.py
+++ wallpy/main.py
@@ -1,3 +1,6 @@
+import requests
+import wget
+
 from wallpy.wallpaper import set_wallpaper
 
 
@@ -6,6 +9,13 @@ def main():
     wallpaper_file_2: str = "images/jonatan-pie-3l3RwQdHRHg-unsplash.jpg"
 
     set_wallpaper(wallpaper_file_2)
+
+    r = requests.get('https://api.nasa.gov/planetary/apod?date=2020-07-25&api_key=DEMO_KEY')
+    r = r.json()
+
+    if r["media_type"] == "image":
+        print(r["url"])
+        wget.download(r["url"], out="image.jpg")
 
 
 if __name__ == '__main__':
