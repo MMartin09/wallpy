dd43c86b27fc51590473a445d76fe2aa06d941fa
MMartin09 <mosermartin09@gmail.com>
2020-07-28 16:00:16 +0200

    Add function to save the apod image to temp file

--- wallpy/main.py
+++ wallpy/main.py
@@ -1,6 +1,9 @@
+import tempfile
+import os
+
 import click
 
-# from wallpy.wallpaper import set_wallpaper
+from wallpy.wallpaper import set_wallpaper
 from wallpy.apod_downloader import download_apod
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
@@ -10,10 +13,18 @@ CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 @click.option("--apod", "-a", show_default=True, is_flag=True)
 def main(apod):
 
+    apod = True
+
     if apod:
-        download_apod("test.jpg")
+        fp = tempfile.TemporaryDirectory()
+        download_apod(os.path.join(fp.name, "test.jpg"))
+        print(os.path.join(fp.name, "test.jpg"))
+        set_wallpaper(os.path.join(fp.name, "test.jpg"))
 
     click.echo("Welcome to wallpy!")
+
+    while True:
+        x = 1
 
 
 if __name__ == "__main__":
