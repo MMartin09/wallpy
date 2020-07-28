2bbea2a7d81793426a2549db882e44c58e459bf2
MMartin09 <mosermartin09@gmail.com>
2020-07-28 08:16:47 +0200

    Create function to download the apod

---
+++ wallpy/apod_downloader.py
@@ -0,0 +1,44 @@
+from enum import Enum, auto
+
+import requests
+import wget
+
+class FILE_TYPE(Enum):
+    IMAGE = auto()
+    VIDEO = auto()
+
+
+def download_apod(out_file) -> None:
+    """Download the APOD from NASA.
+
+    Args:
+        out_file (str): The output file where the image should be written.
+
+    """
+
+    apod = query_apod()
+
+    if not apod_filetype(apod) is FILE_TYPE.IMAGE:
+        raise TypeError("Error: APOD is no image!")
+
+    wget.download(apod["url"], out=out_file)
+
+
+def query_apod() -> dict:
+    """Query the APOD.
+
+    Returns:
+        The query result.
+
+    """
+
+    r = requests.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+
+    return r.json()
+
+
+def apod_filetype(apod) -> FILE_TYPE:
+    if apod["media_type"] == "image":
+        return FILE_TYPE.IMAGE
+    else:
+        return FILE_TYPE.VIDEO
--- wallpy/main.py
+++ wallpy/main.py
@@ -2,20 +2,16 @@ import requests
 import wget
 
 from wallpy.wallpaper import set_wallpaper
+from wallpy.apod_downloader import download_apod
 
 
 def main():
-    wallpaper_file_1: str = "images/breno-machado-in9-n0JwgZ0-unsplash.jpg"
-    wallpaper_file_2: str = "images/jonatan-pie-3l3RwQdHRHg-unsplash.jpg"
+    # wallpaper_file_1: str = "images/breno-machado-in9-n0JwgZ0-unsplash.jpg"
+    # wallpaper_file_2: str = "images/jonatan-pie-3l3RwQdHRHg-unsplash.jpg"
 
-    set_wallpaper(wallpaper_file_2)
+    # set_wallpaper(wallpaper_file_2)
 
-    r = requests.get('https://api.nasa.gov/planetary/apod?date=2020-07-25&api_key=DEMO_KEY')
-    r = r.json()
-
-    if r["media_type"] == "image":
-        print(r["url"])
-        wget.download(r["url"], out="image.jpg")
+    download_apod("test.jpg")
 
 
 if __name__ == '__main__':
