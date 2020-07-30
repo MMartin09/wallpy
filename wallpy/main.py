60012200c7d1c6a9836600f6c0695ae5e9058cfa
MMartin09 <mosermartin09@gmail.com>
2020-07-30 07:48:22 +0200

    Add file option to wallpy

--- wallpy/main.py
+++ wallpy/main.py
@@ -4,34 +4,39 @@ import time
 
 import click
 
-from wallpy.wallpaper import set_wallpaper
-from wallpy.apod_downloader import download_apod, download_bing
-
 from wallpy.url_query import UrlQuery
 from wallpy.image_download import ImageDownload
+from wallpy.wallpaper import set_wallpaper
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.option("--apod", "-a", show_default=True, is_flag=True)
-@click.option("--bing", "-b", show_default=True, is_flag=True)
-def main(apod, bing):
+@click.option(
+    "--apod",
+    "-a",
+    "apod",
+    is_flag=True,
+    help="Download the Astronomy Picture of the Day (APoD",
+)
+@click.option(
+    "--bing", "-b", "bing", is_flag=True, help="Download the Bing imgae of the day"
+)
+@click.option("--file", "-f", "file", help="Use the file as wallpaper")
+def main(apod, bing, file):
     click.echo("Welcome to wallpy!")
 
-    # apod = True
-    bing = True
+    if file is None:
+        fp = tempfile.TemporaryDirectory()
+        file = os.path.join(fp.name, "wallpaper.jpg")
 
-    fp = tempfile.TemporaryDirectory()
-    file = os.path.join(fp.name, "test.jpg")
+        if apod:
+            url = UrlQuery().query("apod")
+            ImageDownload().download(url, file)
 
-    if apod:
-        url = UrlQuery().query("apod")
-        ImageDownload().download(url, file)
-
-    elif bing:
-        url = UrlQuery().query("bing")
-        ImageDownload().download(url, file)
+        elif bing:
+            url = UrlQuery().query("bing")
+            ImageDownload().download(url, file)
 
     set_wallpaper(file)
 
