13709ad3b5cf035fab7374591aae793a7269e2d0
MMartin09 <mosermartin09@gmail.com>
2020-07-31 08:48:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,13 @@ setup(
     author_email="mosermartin09@gmail.com",
     description="A simple wallpaper app",
     entry_points={"console_scripts": ["wallpy=wallpy.main:main"]},
+    install_requires=["click==7.1.2", "requests==2.24.0", "wget==3.2"],
     license="MIT",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     name="wallpy",
     packages=["wallpy"],
+    python_requires=">=3.6",
     url="https://github.com/MMartin09/wallpy",
     version=VERSION,
 )
