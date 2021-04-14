fdcd3476b1366173b3865902c0d281405f4a2044
Moser Martin <m.moser@schreder-cms.com>
2021-04-14 09:59:58 +0200

    Display two images side by side in the main window

--- wallpy/gui/main_window.py
+++ wallpy/gui/main_window.py
@@ -1,7 +1,7 @@
 
 import toga
 from toga.style import Pack
-from toga.style.pack import COLUMN
+from toga.style.pack import COLUMN, ROW, CENTER
 
 
 class MainWindow(toga.App):
@@ -9,17 +9,49 @@ class MainWindow(toga.App):
 
     """
 
+    _imageview_apod: toga.ImageView
+    _imageview_biod: toga.ImageView
+
     def startup(self):
-        self.main_window = toga.MainWindow(title="Wallpy")
+        self.main_window = toga.MainWindow(title="Test")
 
-        box = toga.Box(style=Pack(direction=COLUMN))
+        self.set_apod("./images/breno-machado-in9-n0JwgZ0-unsplash.jpg")
+        self.set_biod("./images/jonatan-pie-3l3RwQdHRHg-unsplash.jpg")
 
-        img = toga.Image(path="./images/jonatan-pie-3l3RwQdHRHg-unsplash.jpg")
-        view = toga.ImageView(img)
+        box_left = toga.Box(
+            children=[
+                toga.Label("Astronomy Picture of the Day"),
+                self._imageview_apod,
+            ],
+            style=Pack(
+                direction=COLUMN,
+                padding_bottom=10,
+                padding_left=10,
+            )
+        )
 
-        box.add(toga.Label("Bing image of the day"))
-        box.add(view)
+        box_right = toga.Box(
+            children=[
+                toga.Label("Bing image of the day"),
+                self._imageview_biod,
+            ],
+            style=Pack(
+                direction=COLUMN,
+                padding_bottom=10,
+                padding_left=10,
+                padding_right=10,
+            )
+        )
 
-        self.main_window.content = box
+        split = toga.SplitContainer()
+        split.content = [(box_left, 0.5), (box_right, 0.5)]
+
+        self.main_window.content = split
 
         self.main_window.show()
+
+    def set_apod(self, path) -> None:
+        self._imageview_apod = toga.ImageView(toga.Image(path))
+
+    def set_biod(self, path) -> None:
+        self._imageview_biod = toga.ImageView(toga.Image(path))
