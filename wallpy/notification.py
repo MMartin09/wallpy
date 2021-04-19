566f1b6873d71cb247ff74593d495cac44da7c57
Moser Martin <m.moser@schreder-cms.com>
2021-04-19 15:30:55 +0200

    Update docstring

--- wallpy/notification.py
+++ wallpy/notification.py
@@ -6,7 +6,18 @@ import System.Windows.Forms as WinForms  # noqa: F401, E402
 from System.Drawing import SystemIcons  # noqa: F401
 
 
-def notificate(title, text, type="exclamation"):
+def notificate(title, text, type="exclamation") -> None:
+    """Show a notification message.
+
+    Currently only "exclamation" type is supported.
+
+    Args:
+        title (str): Title of the notification
+        text (str): Text of the notification
+        type (str): Type of the notification (Used for the notification icon)
+
+    """
+
     toast = WinForms.NotifyIcon()
 
     if type == "exclamation":
